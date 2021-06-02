ead75a7bb253e633512f141ab6adfa88a0abffd2
patrickverdeja <84103820+patrickverdeja@users.noreply.github.com>
2021-06-02 14:26:31 -0700

    Add files via upload

---
+++ p6.js
@@ -0,0 +1,142 @@
+/* Patrick Verdeja Herms
+// Project 6
+CIT281 Colbert */
+
+class Shape {
+  constructor(sides = []) {
+    this.sides = sides;
+  }
+  perimeter() {
+    return this.sides.reduce((total, num) => total + num, 0);
+  }
+}
+
+// console.log(new Shape([5, 10]).perimeter());  // 15
+// console.log(new Shape([1, 2, 3, 4, 5]).perimeter()); // 15
+// console.log(new Shape().perimeter()); // 0
+
+class Rectangle extends Shape {
+  constructor(length = 0, width = 0) {
+    super([length, width, length, width]);
+    this.length = length;
+    this.width = width;
+  }
+  area() {
+    return this.length * this.width;
+  }
+}
+
+// console.log(new Rectangle(4, 4).perimeter());  // 16
+// console.log(new Rectangle(4, 4).area());  // 16
+// console.log(new Rectangle(5, 5).perimeter()); // 20
+// console.log(new Rectangle(5, 5).area()); // 25
+// console.log(new Rectangle().perimeter()); // 0
+// console.log(new Rectangle().area()); // 0
+
+class Triangle extends Shape {
+  constructor(sideA = 0, sideB = 0, sideC = 0) {
+    super([sideA, sideB, sideC]);
+    this.sideA = sideA;
+    this.sideB = sideB;
+    this.sideC = sideC;
+  }
+  area() {
+    let s = this.perimeter() / 2;
+    let a = Math.sqrt(
+      s * (s - this.sideA) * (s - this.sideB) * (s - this.sideC)
+    );
+    return a;
+  }
+}
+
+// console.log(new Triangle(3, 4, 5).perimeter()); // 12
+// console.log(new Triangle(3, 4, 5).area()); // 6
+// console.log(new Triangle().perimeter()); // 0
+// console.log(new Triangle().area()); // 0
+
+// Array of sides arrays
+const data = [[3, 4], [5, 5], [3, 4, 5], [10], []];
+
+for (let sides of data) {
+  let newShape;
+  let shapeType;
+  switch (sides.length) {
+    case 2:
+      if (sides[0] === sides[1]) {
+        shapeType = "Square";
+      } else {
+        shapeType = "Rectangle";
+      }
+      newShape = new Rectangle(...sides);
+      console.log(
+        `${shapeType} with sides ${sides} has a perimeter of ${newShape.perimeter()} and an area of ${newShape.area()}`
+      );
+      break;
+    case 3:
+      shapeType = "Triangle";
+      newShape = new Triangle(...sides);
+      console.log(
+        `${shapeType} with sides ${sides} has a perimeter of ${newShape.perimeter()} and an area of ${newShape.area()}`
+      );
+      break;
+    case 1:
+      console.log(`Shape with ${sides.length} side unsupported`);
+      break;
+    default:
+      console.log(`Shape with ${sides.length} sides unsupported`);
+  }
+}
+
+Switch statement as if elseif statement
+
+if (sides.length === 2) {
+  if (sides[0] === sides[1]) {
+    shapeType = "Square";
+  } else {
+    shapeType = "Rectangle";
+  }
+  newShape = new Rectangle(...sides);
+  console.log(
+    `${shapeType} with sides ${sides} has a perimeter of ${newShape.perimeter()} and an area of ${newShape.area()}`
+  );
+} else if (sides.length === 3) {
+  shapeType = "Triangle";
+  newShape = new Triangle(...sides);
+  console.log(
+    `${shapeType} with sides ${sides} has a perimeter of ${newShape.perimeter()} and an area of ${newShape.area()}`
+  );
+} else if (sides.length === 1) {
+  console.log(`Shape with ${sides.length} side unsupported`);
+} else {
+  console.log(`Shape with ${sides.length} sides unsupported`);
+}
+
+for (let sides of data) {
+  let newShape;
+  let shapeType;
+  switch (sides.length) {
+    case 2:
+      if (sides[0] === sides[1]) {
+        shapeType = "Square";
+      } else {
+        shapeType = "Rectangle";
+      }
+      newShape = new Rectangle(...sides);
+      console.log(
+        `${shapeType} with sides ${sides} has a perimeter of ${newShape.perimeter()} and an area of ${newShape.area()}`
+      );
+      break;
+    case 3:
+      shapeType = "Triangle";
+      newShape = new Triangle(...sides);
+      console.log(
+        `${shapeType} with sides ${sides} has a perimeter of ${newShape.perimeter()} and an area of ${newShape.area()}`
+      );
+      break;
+    case 1:
+      console.log(`Shape with ${sides.length} side unsupported`);
+      break;
+    default:
+      console.log(`Shape with ${sides.length} sides unsupported`);
+  }
+}
